c75dfa6152cf4d56b80329df548a09b0fa442868
ROVIC21 <rovicmaislim130@gmail.com>
2024-10-04 10:40:23 +0800

    new code

--- OOP Project/script.js
+++ OOP Project/script.js
@@ -1,50 +1,59 @@
-// Person Class Definition 
-class Person { 
-    #name; 
-    #age; 
-    #occupation; 
- 
-    constructor(name, age, occupation) { 
-        this.#name = name; 
-        this.#age = age; 
-        this.#occupation = occupation; 
-    } 
- 
-    getName() { 
-        return this.#name; 
-    } 
- 
-    getAge() { 
-        return this.#age; 
-    } 
- 
-    getOccupation() { 
-        return this.#occupation; 
-    } 
-} 
- 
-// Student Class Definition 
-class Student extends Person { 
-    #major; 
- 
-    constructor(name, age, occupation, major) { 
-        super(name, age, occupation); 
-        this.#major = major; 
-    } 
- 
-    getMajor() { 
-        return this.#major; 
-    } 
-} 
- 
-// Create a Student instance 
-const student = new Student("March", 14, "Instructor", "Programming"); 
- 
-// Display student information in the div 
-const studentInfoDiv = document.getElementById("student-info"); 
-studentInfoDiv.innerHTML = ` 
-    <p><strong>Name:</strong> ${student.getName()}</p> 
-    <p><strong>Age:</strong> ${student.getAge()}</p> 
-    <p><strong>Occupation:</strong> ${student.getOccupation()}</p> 
-    <p><strong>Major:</strong> ${student.getMajor()}</p> 
-`;
+class Person {
+    #name;
+    #age;
+    #occupation;
+
+    constructor(name, age, occupation) {
+        this.#name = name;
+        this.#age = age;
+        this.#occupation = occupation;
+    }
+
+
+    getName() {
+        return this.#name;
+    }
+
+
+    getAge() {
+        return this.#age;
+    }
+   getOccupation() {
+        return this.#occupation;
+    }
+}
+
+
+class Student extends Person {
+    constructor(name, age, occupation, major) {
+        super(name, age, occupation);
+        this.major = major;
+    }
+
+
+    displayInfo() {
+        return `
+            Name: ${this.getName()}<br>
+            Age: ${this.getAge()}<br>
+            Occupation: ${this.getOccupation()}<br>
+            Major: ${this.major}
+        `;
+    }
+
+    
+    logInfo() {
+        console.log(`Name: ${this.getName()}`);
+        console.log(`Age: ${this.getAge()}`);
+        console.log(`Occupation: ${this.getOccupation()}`);
+        console.log(`Major: ${this.major}`);
+    }
+}
+
+
+const student = new Student("MAISLIM,ROVIC", 20, "Student", "Object Oriented Programming");
+
+
+document.getElementById("student-info").innerHTML = student.displayInfo();
+
+
+student.logInfo();
